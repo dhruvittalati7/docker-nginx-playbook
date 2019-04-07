62d74ed0c2338065d481571c88a1ba3a519e47ca
root <root@ip-172-31-87-213.ec2.internal>
2019-04-07 12:19:39 +0000

    fixed shell script

--- startPlaybooks.sh
+++ startPlaybooks.sh
@@ -1,3 +1,6 @@
+# Some image versions don't contain python, so we need to install it
+docker exec -it debian /bin/sh -c "apt-get update && apt-get install python -y"
+
 names=$(docker container ls --format '{{.Names}}')
 
 for name in $names
